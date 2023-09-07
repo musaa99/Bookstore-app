365e2e7535a8c872036ccdc048c04031005234a9
Musa Opaluwa <innovate.musa.opaluwa@gmail.com>
2023-09-07 14:12:09 +0100

    fixed thr toolkit page

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,24 @@
 /* eslint-disable react/jsx-key */
 // import {useState} from 'react';
-import BookstoreApp from './BookStore';
-import Link from 'next/link';
+// import BookstoreApp from './BookStore';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Link from "next/link";
+import { AddBook } from "./component/addBook";
+import BookForm from "./component/BookForm";
+import Homepage from "./component/home";
+import BookList from "./component/BookList";
 // import axios from 'axios';
 
 export default function Home() {
-
-	
-	return (
-		<div className='m-auto '>
-
-			<BookstoreApp/>
-		</div>
-
-	);
+  return (
+    // <Router>
+    //   <Switch>
+    //     <Route exact path="/home" component={Homepage} />
+    //      <Route path="/booklist" component={BookList} /> 
+    //   </Switch>
+    // </Router>
+	<div>
+		<AddBook/>
+	</div>
+  );
 }
