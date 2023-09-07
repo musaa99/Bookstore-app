0724c25b2cab2c1d9420c944df363aaa4ab32504
Musa Opaluwa <innovate.musa.opaluwa@gmail.com>
2023-09-07 14:22:09 +0100

    added axios and modify book form and list components

--- pages/component/BookList.jsx
+++ pages/component/BookList.jsx
@@ -1,15 +1,44 @@
-import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { deleteBook, fetchBooks } from '../slices/booksSlice';
+import { deleteBookById } from '../services/api';
 
-const BookList = ({ books, deleteBook }) => {
+const BookList = () => {
+  const dispatch = useDispatch();
+  const books = useSelector((state) => state.books);
+
+  useEffect(() => {
+    const getBooks = async () => {
+      try {
+        const booksData = await fetchBooks();
+        dispatch(fetchBooks(booksData));
+      } catch (error) {
+        console.error('Error fetching books:', error);
+      }
+    };
+    getBooks();
+  }, [dispatch]);
+
+  const handleDeleteBook = async (bookId) => {
+    try {
+      await deleteBookById(bookId);
+      dispatch(deleteBook(bookId));
+    } catch (error) {
+      console.error('Error deleting book:', error);
+    }
+  };
+
   return (
-    <ul>
-      {books.map((book, index) => (
-          <div className='text-center' key={index}>
-          <div>{book.name} by {book.author} ({book.year})</div>
-          <button className='flex  text-center m-auto bg-[red] rounded-[50px] px-4' onClick={() => deleteBook(index)}>Delete</button>
-        </div>
-      ))}
-    </ul>
+    <div>
+      <ul>
+        {books.map((book) => (
+          <li key={book.id}>
+            {book.title}{' '}
+            <button onClick={() => handleDeleteBook(book.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
--- pages/component/addBook.jsx
+++ pages/component/addBook.jsx
@@ -1,89 +1,35 @@
-import { useDispatch, useSelector } from "react-redux";
-import { useRouter } from "next/navigation";
-import { useState } from "react";
-import { addBooks } from "../redux/booksSlice";
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addBook } from '../redux/booksSlice';
+import { addNewBook } from '../api/api';
 
-export function AddBook() {
+const AddBookForm = () => {
+  const [title, setTitle] = useState('');
   const dispatch = useDispatch();
-  const router = useRouter();
 
-  const [name, setName] = useState("");
-  const [author, setAuthor] = useState("");
-  const [year, setYear] = useState("");
-  const [error, setError] = useState(null);
+  const handleAddBook = async () => {
+    if (!title) return;
 
-  const handleName = (e) => setName(e.target.value);
-  const handleAuthor = (e) => setAuthor(e.target.value);
-  const handleYear = (e) => setYear(e.target.value);
-
-  //  const usersAmount = useSelector((state) => state.users.entities.length);
-
-  const handleClick = () => {
-    if (name && author && year) {
-      dispatch(
-        addBooks({
-          name,
-          year,
-          author,
-        })
-      );
-
-      setError(null);
-      router.push("/Booklist");
-    } else {
-      setError("Fill in all fields");
+    try {
+      const newBook = await addNewBook({ title });
+      dispatch(addBook(newBook));
+      setTitle('');
+    } catch (error) {
+      console.error('Error adding book:', error);
     }
-
-    const newBook = { name, author, year };
-    newBook(newBook);
-    setName('');
-    setAuthor('');
-    setYear('');
   };
 
   return (
-    <div className="text-center m-4">
-      <div className="">
-        <h1>Add Book</h1>
-      </div>
-      <div className="flex justify-center flex-cols items-center">
-        <div className="grid">
-          <label htmlFor="nameInput">Name</label>
-          <input
-            className="shadow appearance-none border rounded  py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            type="text"
-            placeholder=""
-            id="nameInput"
-            onChange={handleName}
-            value={name}
-          />
-          <label htmlFor="nameInput">Author</label>
-          <input
-            className="shadow appearance-none border rounded  py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            type="text"
-            // placeholder="test@mailbox.com"
-            id="nameInput"
-            onChange={handleAuthor}
-            value={author}
-          />
-          <label htmlFor="emailInput">Year</label>
-          <input
-            className="shadow appearance-none border rounded  py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            type="text"
-            placeholder=""
-            id="yearInput"
-            onChange={handleYear}
-            value={year}
-          />
-          {error && error}
-          <button
-            onClick={handleClick}
-            className=" mt-[14px]  text-center w-full m-auto bg-[blue] rounded-[10px] px-4"
-          >
-            Add Book
-          </button>
-        </div>
-      </div>
+    <div>
+      <input
+        type="text"
+        placeholder="Enter book title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <button onClick={handleAddBook}>Add Book</button>
     </div>
   );
-}
+};
+
+export default AddBookForm;
